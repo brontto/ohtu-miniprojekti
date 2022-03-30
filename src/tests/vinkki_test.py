06929ef0c86f0e345fc92cf6da70e03bb2031dd5
2laJ2 <tuulajakobsson@hotmail.com>
2022-03-30 12:54:30 +0300

    add yksi testi

--- src/tests/vinkki_test.py
+++ src/tests/vinkki_test.py
@@ -1,5 +1,4 @@
 import unittest
-from optparse import make_option
 from vinkki import Vinkki
 
 
@@ -7,6 +6,5 @@ class TestMain(unittest.TestCase):
     def setUp(self):
         self.vinkki = Vinkki()
 
-    def viesti(self):
+    def testaa_vastaus(self):
         self.assertEqual(self.vinkki.vastaus(), "Hello Hello!")
-
