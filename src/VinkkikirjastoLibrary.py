f03c55ad98e61af9cfaf61cda7ebbbc189dc537d
brontto <bronttosaur@gmail.com>
2022-03-31 13:01:48 +0300

    pylintia varten fixailuja

--- src/VinkkikirjastoLibrary.py
+++ src/VinkkikirjastoLibrary.py
@@ -1,13 +1,13 @@
 from vinkkikirjasto import Vinkkikirjasto
 
+
 class VinkkikirjastoLibrary:
     def __init__(self):
         self._vinkkikirjasto = Vinkkikirjasto()
-    
         
     def lisaa_uusi_vinkki(self, otsikko, url):
-        self._vinkkikirjasto.lisaa_uusi_vinkki(otsikko,url)
-        
+        self._vinkkikirjasto.lisaa_uusi_vinkki(otsikko, url)
+
     def uuden_vinkin_otsikko_pitaisi_olla(self, otsikko):
         viimeisin_otsikko = self._vinkkikirjasto.hea_viimeksi_lisatty_vinkki().get_otsikko()
         if viimeisin_otsikko != otsikko:
