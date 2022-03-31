9c35cfc386a47e4b9c2711af3968ed47a0f2fb26
brontto <bronttosaur@gmail.com>
2022-03-31 13:07:17 +0300

    pylint fix

--- src/VinkkikirjastoLibrary.py
+++ src/VinkkikirjastoLibrary.py
@@ -4,7 +4,7 @@ from vinkkikirjasto import Vinkkikirjasto
 class VinkkikirjastoLibrary:
     def __init__(self):
         self._vinkkikirjasto = Vinkkikirjasto()
-        
+
     def lisaa_uusi_vinkki(self, otsikko, url):
         self._vinkkikirjasto.lisaa_uusi_vinkki(otsikko, url)
 
@@ -12,3 +12,4 @@ class VinkkikirjastoLibrary:
         viimeisin_otsikko = self._vinkkikirjasto.hea_viimeksi_lisatty_vinkki().get_otsikko()
         if viimeisin_otsikko != otsikko:
             raise AssertionError(f"{viimeisin_otsikko} != {otsikko}")
+        
