f9918098f8031a039d98575f7a1ad67a0ba75e0f
Maijjay <maija.ylitalo95@gmail.com>
2022-04-06 16:38:59 +0300

    Kirjautumisen alustava html-pohja ja flask-route

--- src/app.py
+++ src/app.py
@@ -12,15 +12,29 @@ app = Flask(__name__)
 vinkkikirjasto = Vinkkikirjasto()
 vinkkikirjasto.esimerkki_vinkit()
 
-@app.route("/", methods=["GET"])
-def render_etusivu():
-    vinkkilista = vinkkikirjasto.hae_kaikki_vinkit()
-    return render_template("index.html", vinkkilista = vinkkilista)
-
 @app.route("/luo_vinkki", methods=["POST"])
 def luo_vinkki():
     otsikko = request.form["otsikko"]
     url = "http://" + request.form["url"]
     print(url)
     vinkkikirjasto.lisaa_uusi_vinkki(otsikko, url)
-    return redirect("/")
+    return redirect("/lukuvinkit")
+
+@app.route("/kirjautuminen", methods=["POST"])
+def kirjautuminen():
+    # kayttajatunnus = request.form["kayttajatunnus"]
+    # salasana = request.form["salasana"]
+
+    # Tarkasta onko käyttäjää olemassa ja onko salasana oikein
+    # Uudelleenohjaus takaisin etusivulle tai lukuvinkkeihin
+    return redirect("/lukuvinkit")
+
+@app.route("/lukuvinkit", methods=["GET"])
+def render_lukuvinkit():
+    vinkkilista = vinkkikirjasto.hae_kaikki_vinkit()
+    return render_template("lukuvinkit.html", vinkkilista = vinkkilista)
+
+
+@app.route("/", methods=["GET"])
+def render_etusivu():
+    return render_template("etusivu.html")
