210aab355fddbc793d7f6127629485e21eb6e3c0
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-04-12 18:04:12 +0300

    Eriytetty sovelluksen luonti ja routet

--- src/app.py
+++ src/app.py
@@ -1,77 +1,17 @@
 from os import getenv
 from dotenv import load_dotenv
-from flask import (
-    Flask,
-    render_template,
-    request,
-    redirect
-)
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask
 from db import db
 
-from vinkkikirjasto import Vinkkikirjasto
-from kayttajat import Kayttajat
 
 app = Flask(__name__)
 
 load_dotenv()
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+import routes
 
-vinkkikirjasto = Vinkkikirjasto()
-kayttajat= Kayttajat()
 
-@app.route("/luo_vinkki", methods=["POST"])
-def luo_vinkki():
-    otsikko = request.form["otsikko"]
-    url = "http://" + request.form["url"]
-    vinkkikirjasto.lisaa_uusi_vinkki(otsikko, url)
-    return redirect("/lukuvinkit")
 
-@app.route("/kirjautuminen", methods=["POST"])
-def kirjautuminen():
-    kayttajatunnus = request.form["kayttajatunnus"]
-    salasana = request.form["salasana"]
-    kayttaja = kayttajat.tarkasta_kayttajatunnus(kayttajatunnus)
-
-    if not kayttaja:
-        print("Käyttäjätunnus on väärin")
-        return redirect("/")
-
-    hash_value = kayttaja.salasana
-    if check_password_hash(hash_value, salasana):
-        print("Kirjauduttu sisään")
-        return redirect("/lukuvinkit")
-    print("Salasana on väärin")
-    return redirect("/")
-
-@app.route("/rekisterointi", methods=["GET"])
-def rekisterointi():
-    return render_template("rekisterointi.html")
-
-@app.route("/luo_uusi_kayttaja", methods=["POST"])
-def luo_uusi_kayttaja():
-    kayttajatunnus = request.form["kayttajatunnus"]
-    salasana = request.form["salasana"]
-    salasana2 = request.form["salasana_varmistus"]
-
-    onkojoolemassa = kayttajat.tarkasta_kayttajatunnus(kayttajatunnus)
-
-    if onkojoolemassa:
-        error = "käyttäjätunnus on jo olemassa"
-        return render_template("rekisterointi.html", error=error)
-
-    if salasana == salasana2:
-        hash_salasana = generate_password_hash(salasana)
-        kayttajat.lisaa_uusi_kayttaja(kayttajatunnus,hash_salasana)
-    return redirect("/")
-
-@app.route("/lukuvinkit", methods=["GET"])
-def render_lukuvinkit():
-    vinkkilista = vinkkikirjasto.hae_kaikki_vinkit()
-    return render_template("lukuvinkit.html", vinkkilista = vinkkilista)
-
-@app.route("/")
-def render_etusivu():
-    return render_template("etusivu.html")
---
+++ src/routes.py
@@ -0,0 +1,63 @@
+from flask import request, redirect, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+from vinkkikirjasto import Vinkkikirjasto
+from kayttajat import Kayttajat
+from app import app
+
+
+vinkkikirjasto = Vinkkikirjasto()
+kayttajat = Kayttajat()
+
+@app.route("/luo_vinkki", methods=["POST"])
+def luo_vinkki():
+    otsikko = request.form["otsikko"]
+    url = "http://" + request.form["url"]
+    vinkkikirjasto.lisaa_uusi_vinkki(otsikko, url)
+    return redirect("/lukuvinkit")
+
+@app.route("/kirjautuminen", methods=["POST"])
+def kirjautuminen():
+    kayttajatunnus = request.form["kayttajatunnus"]
+    salasana = request.form["salasana"]
+    kayttaja = kayttajat.tarkasta_kayttajatunnus(kayttajatunnus)
+
+    if not kayttaja:
+        print("Käyttäjätunnus on väärin")
+        return redirect("/")
+
+    hash_value = kayttaja.salasana
+    if check_password_hash(hash_value, salasana):
+        print("Kirjauduttu sisään")
+        return redirect("/lukuvinkit")
+    print("Salasana on väärin")
+    return redirect("/")
+
+@app.route("/rekisterointi", methods=["GET"])
+def rekisterointi():
+    return render_template("rekisterointi.html")
+
+@app.route("/luo_uusi_kayttaja", methods=["POST"])
+def luo_uusi_kayttaja():
+    kayttajatunnus = request.form["kayttajatunnus"]
+    salasana = request.form["salasana"]
+    salasana2 = request.form["salasana_varmistus"]
+
+    tunnus_olemassa = kayttajat.tarkasta_kayttajatunnus(kayttajatunnus)
+
+    if tunnus_olemassa:
+        error = "käyttäjätunnus on jo olemassa"
+        return render_template("rekisterointi.html", error=error)
+
+    if salasana == salasana2:
+        hash_salasana = generate_password_hash(salasana)
+        kayttajat.lisaa_uusi_kayttaja(kayttajatunnus,hash_salasana)
+    return redirect("/")
+
+@app.route("/lukuvinkit", methods=["GET"])
+def render_lukuvinkit():
+    vinkkilista = vinkkikirjasto.hae_kaikki_vinkit()
+    return render_template("lukuvinkit.html", vinkkilista = vinkkilista)
+
+@app.route("/")
+def render_etusivu():
+    return render_template("etusivu.html")
