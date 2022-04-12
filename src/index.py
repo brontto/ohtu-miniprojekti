2f8899aaaca6021e040951e2d8dea821f1513b8e
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-04-12 17:09:17 +0300

    Poistettu käyttämätön Vinkki-luokka

--- src/index.py
+++ src/index.py
@@ -1,19 +1,6 @@
-from vinkki import Vinkki
 from app import app
-def main():
-
-    def viesti():
-        print ("Hello Codefairy!")
-
-    viesti()
-
-    vinkki = Vinkki()
-
-    print(vinkki.vastaus())
-
-    print(vinkki.huomenta())
-
+  
+ 
 if __name__ == "__main__":
-    main()
     app.run()
 
--- src/vinkki.py
+++
@@ -1,9 +0,0 @@
-class Vinkki():
-    def __init__(self):
-        pass
-
-    def vastaus(self):
-        return "Hello Hello!"
-
-    def huomenta(self):
-        return "Good Morning, Rise'n Shine Today!"
