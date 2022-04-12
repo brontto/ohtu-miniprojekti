9976a30807b3725a3fdb4a0b6ebb8270c49adac2
brontto <bronttosaur@gmail.com>
2022-04-12 10:24:49 +0300

    main branch works normally

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ from kayttajat import Kayttajat
 app = Flask(__name__)
 
 load_dotenv()
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL').replace("://", "ql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL')
 db.init_app(app)
 
 
