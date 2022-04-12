2219a130ab9164565401bae42df267422558b819
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-04-12 18:41:19 +0300

    App-moduulin rakenne

--- src/app.py
+++ src/app.py
@@ -3,12 +3,16 @@ from dotenv import load_dotenv
 from flask import Flask
 from db import db
 
+def create_app():
+    flask_app = Flask(__name__)
 
-app = Flask(__name__)
+    load_dotenv()
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = \
+        getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(flask_app)
+    return flask_app
 
-load_dotenv()
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
+app = create_app()
 
 import routes # pylint: disable=unused-import, wrong-import-position, cyclic-import
