97c38da6e3a5bfb19afb81d1b5deed1168bcaf2a
2laJ2 <tuulajakobsson@hotmail.com>
2022-04-14 15:56:56 +0300

    otsikko ja linkki pakolliset

--- src/vinkkikirjasto.py
+++ src/vinkkikirjasto.py
@@ -1,3 +1,4 @@
+from sqlalchemy import false, true
 from lukuvinkki import Lukuvinkki
 from vinkki_repository import vinkkirepositorio as default_vinkkirepo
 
@@ -18,9 +19,20 @@ class Vinkkikirjasto:
         return self.vinkit.hae_uusin_vinkki()
 
     def lisaa_uusi_vinkki(self, otsikko, url):
-        uusi_vinkki = Lukuvinkki(otsikko, url)
-        self.vinkit.lisaa_uusi_vinkki(uusi_vinkki)
-
+        if self.onko_otsikko_kelvollinen(otsikko)==True and self.onko_url_kelvollinen(url)==True:
+            uusi_vinkki = Lukuvinkki(otsikko, url)
+            self.vinkit.lisaa_uusi_vinkki(uusi_vinkki)
+        
     def poista_kaikki_vinkit(self):
         self.vinkit.poista_kaikki_vinkit()
+        
+    def onko_url_kelvollinen(self, url):
+        url = url.strip()
+        if len(url)>7:
+            return True
+     
+    def onko_otsikko_kelvollinen(self, otsikko): 
+        otsikko = otsikko.strip()
+        if len(otsikko)!=0:
+            return True
         
